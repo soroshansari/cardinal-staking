c8fc27e22e144a7149f5264cf0a93f71545a3ed7
Giannis Chatziveroglou <gchatz@mit.edu>
2022-06-30 20:45:39 +0300

    Change update ixs

--- programs/cardinal-stake-pool/src/instructions/update_pool.rs
+++ programs/cardinal-stake-pool/src/instructions/update_pool.rs
@@ -2,16 +2,16 @@ use {crate::state::*, anchor_lang::prelude::*};
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct UpdatePoolIx {
-    overlay_text: Option<String>,
     image_uri: Option<String>,
-    requires_collections: Option<Vec<Pubkey>>,
-    requires_creators: Option<Vec<Pubkey>>,
-    requires_authorization: Option<bool>,
-    authority: Option<Pubkey>,
-    reset_on_stake: Option<bool>,
-    cooldown_seconds: Option<u32>,
-    min_stake_seconds: Option<u32>,
-    end_date: Option<i64>,
+    overlay_text: String,
+    requires_collections: Vec<Pubkey>,
+    requires_creators: Vec<Pubkey>,
+    requires_authorization: bool,
+    authority: Pubkey,
+    reset_on_stake: bool,
+    cooldown_seconds: u32,
+    min_stake_seconds: u32,
+    end_date: i64,
 }
 
 #[derive(Accounts)]
@@ -26,22 +26,16 @@ pub struct UpdatePoolCtx<'info> {
 
 pub fn handler(ctx: Context<UpdatePoolCtx>, ix: UpdatePoolIx) -> Result<()> {
     let stake_pool = &mut ctx.accounts.stake_pool;
-    stake_pool.requires_collections = ix.requires_collections.unwrap_or_else(|| stake_pool.requires_collections.clone());
-    stake_pool.requires_creators = ix.requires_creators.unwrap_or_else(|| stake_pool.requires_creators.clone());
-    stake_pool.requires_authorization = ix.requires_authorization.unwrap_or(stake_pool.requires_authorization);
-    stake_pool.overlay_text = ix.overlay_text.unwrap_or_else(|| stake_pool.overlay_text.clone());
+    stake_pool.requires_collections = ix.requires_collections;
+    stake_pool.requires_creators = ix.requires_creators;
+    stake_pool.requires_authorization = ix.requires_authorization;
+    stake_pool.overlay_text = ix.overlay_text;
+    stake_pool.authority = ix.authority;
+    stake_pool.reset_on_stake = ix.reset_on_stake;
+    stake_pool.end_date = Some(ix.end_date);
+    stake_pool.cooldown_seconds = Some(ix.cooldown_seconds);
+    stake_pool.min_stake_seconds = Some(ix.min_stake_seconds);
     stake_pool.image_uri = ix.image_uri.unwrap_or_else(|| stake_pool.image_uri.clone());
-    stake_pool.authority = ix.authority.unwrap_or(stake_pool.authority);
-    stake_pool.reset_on_stake = ix.reset_on_stake.unwrap_or(stake_pool.reset_on_stake);
-    if ix.cooldown_seconds.is_some() {
-        stake_pool.cooldown_seconds = ix.cooldown_seconds;
-    }
-    if ix.min_stake_seconds.is_some() {
-        stake_pool.min_stake_seconds = ix.min_stake_seconds;
-    }
-    if ix.end_date.is_some() {
-        stake_pool.end_date = ix.end_date;
-    }
 
     // zero extra data
     let stake_pool_account = stake_pool.to_account_info();
