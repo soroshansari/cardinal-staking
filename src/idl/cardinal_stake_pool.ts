c2f4857dbdb9fe43fd6d452c2f8f166849fd6951
jpbogle <jpbogle22@gmail.com>
2022-07-01 01:28:33 -0400

    Bump version

--- src/idl/cardinal_stake_pool.ts
+++ src/idl/cardinal_stake_pool.ts
@@ -1,5 +1,5 @@
 export type CardinalStakePool = {
-  version: "1.5.25";
+  version: "1.5.30";
   name: "cardinal_stake_pool";
   instructions: [
     {
@@ -946,7 +946,7 @@ export type CardinalStakePool = {
 };
 
 export const IDL: CardinalStakePool = {
-  version: "1.5.25",
+  version: "1.5.30",
   name: "cardinal_stake_pool",
   instructions: [
     {
