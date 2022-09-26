a840c55c4228f9eada126e298c158f4029c1e456
Giannis Chatziveroglou <gchatz@mit.edu>
2022-09-26 15:54:25 -0700

    bump version

--- src/idl/cardinal_stake_pool.ts
+++ src/idl/cardinal_stake_pool.ts
@@ -1,5 +1,5 @@
 export type CardinalStakePool = {
-  version: "1.9.0";
+  version: "1.10.0";
   name: "cardinal_stake_pool";
   instructions: [
     {
@@ -1042,7 +1042,7 @@ export type CardinalStakePool = {
 };
 
 export const IDL: CardinalStakePool = {
-  version: "1.9.0",
+  version: "1.10.0",
   name: "cardinal_stake_pool",
   instructions: [
     {
