fa42987e3d309e0881b53e618902c41b01f372c9
jpbogle <jpbogle22@gmail.com>
2022-10-03 18:33:26 -0400

    Check zeros script

---
+++ tools/checkZeros.ts
@@ -0,0 +1,56 @@
+import { BorshAccountsCoder, utils } from "@project-serum/anchor";
+import type { Connection } from "@solana/web3.js";
+
+import type { RewardDistributorData } from "../src/programs/rewardDistributor";
+import {
+  REWARD_DISTRIBUTOR_ADDRESS,
+  REWARD_DISTRIBUTOR_IDL,
+} from "../src/programs/rewardDistributor";
+import { connectionFor } from "./connection";
+
+const CLUSTER = "mainnet";
+// const MAX_SIZE = 400;
+
+export const getAllRewardDistributors = async (connection: Connection) => {
+  const programAccounts = await connection.getProgramAccounts(
+    REWARD_DISTRIBUTOR_ADDRESS,
+    {
+      filters: [
+        {
+          memcmp: {
+            offset: 0,
+            bytes: utils.bytes.bs58.encode(
+              BorshAccountsCoder.accountDiscriminator("rewardDistributor")
+            ),
+          },
+        },
+      ],
+    }
+  );
+  return programAccounts;
+};
+
+const checkZeros = async (cluster: string) => {
+  const connection = connectionFor(cluster);
+  const coder = new BorshAccountsCoder(REWARD_DISTRIBUTOR_IDL);
+  const allRewardDistributors = await getAllRewardDistributors(connection);
+  console.log(
+    `--------- Check zeros ${allRewardDistributors.length} reward distributos ---------`
+  );
+  for (let i = 0; i < allRewardDistributors.length; i++) {
+    const r = allRewardDistributors[i]!;
+    const rewardDistributorData: RewardDistributorData = coder.decode(
+      "rewardDistributor",
+      r.account.data
+    );
+    const encoded = await coder.encode(
+      "rewardDistributor",
+      rewardDistributorData
+    );
+    if (r.account.data.slice(encoded.length).some((b) => b !== 0)) {
+      console.log(r.pubkey.toString(), rewardDistributorData);
+    }
+  }
+};
+
+checkZeros(CLUSTER).catch((e) => console.log(e));
